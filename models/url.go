6ad6243b4c5856e7309a203a85770ae619147fec
Slim-Hady <mohamed.hady.dev@gmail.com>
2026-06-08 01:12:22 +0300

    fix: fix spelling

--- models/url.go
+++ models/url.go
@@ -1,12 +1,13 @@
 package models
+
 /*
-this file define the DSA so i the system is like that 
-Long Link--req---> [ Shortener ] --res---> Short Link 
+this file define the DSA so i the system is like that
+Long Link--req---> [ Shortener ] --res---> Short Link
 */
 type ShortenerRequest struct {
 	URL string `json:"url"`
 }
 
-type ShorenerResponse struct { 
-	ShortURL string `json;"short_url"`
+type ShortenerResponse struct {
+	ShortURL string `json:"short_url"`
 }
