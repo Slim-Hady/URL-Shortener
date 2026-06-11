618f803d27e1d51576c46f4039d6b71176f58075
Slim-Hady <mohamed.hady.dev@gmail.com>
2026-06-11 22:42:16 +0300

    add handlers for redirect the URL and make short one

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
-package urlshortener
+package main
+
+import(
+	"fmt"
+	"URL-SHORTENER/storage"
+)	
+
+func main(){
+	store := storage.NewRedisStore()
+
+	originalURL := "https://google.com"
+
+	shortURL, err := store.SaveURL(originalURL)
+
+	if err != nil {
+		fmt.Println("Error occuer", err)
+		return 
+	}
+	fmt.Println("ShortURL saved " , shortURL)
+	originURL, err := store.GetOriginURL(shortURL)
+	if err != nil { 
+		fmt.Println("Error occuer", err)
+		return
+	}
+	fmt.Println("Oringinal URL ",originURL)
+	
+}
