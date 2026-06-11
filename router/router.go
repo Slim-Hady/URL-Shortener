3d6745974324597057935ba0d700f4a8b4be8d02
Slim-Hady <mohamed.hady.dev@gmail.com>
2026-06-12 01:30:34 +0300

    feat: Making the main server on host 8080 and making the get and post

--- router/router.go
+++ router/router.go
@@ -1,2 +1,14 @@
 package router
 
+import(
+	"fmt"
+	"URL-SHORTENER/handlers"
+	"net/http"
+)
+
+func Start(){
+	http.HandleFunc("/shorten", handlers.ShortenURL)
+	http.HandleFunc("/", handlers.RedirectURL)
+	fmt.Println("Server running on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
