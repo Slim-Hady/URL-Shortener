6cc41168d779c54d5244cc2442a1cbff740fffe9
Slim-Hady <mohamed.hady.dev@gmail.com>
2026-06-12 01:30:16 +0300

    Fix: Make the main domain LocalHost

--- storage/redis-store.go
+++ storage/redis-store.go
@@ -32,19 +32,23 @@ func (s *RedisStore) SaveURL(originalURL string) (string, error){
 	h := fnv.New64a()
 	h.Write([]byte(originalURL))
 	//fmt.Print("The hashing using sum64 ",h.Sum64(), "\n")
+	
 	hashURLtoString := fmt.Sprintf("%x" , h.Sum64()); // this is using convert to Hex string, Conver to decimal using '%d' 
+	
+	shorturl := hashURLtoString
+	shorturl = shorturl[:6]
 
-	err := s.Client.Set(ctx, originalURL,hashURLtoString,0).Err()
+	err := s.Client.Set(ctx, originalURL,shorturl,0).Err()
 	if err != nil {
 		return "" , err
 	}
 
-	err = s.Client.Set(ctx,hashURLtoString , originalURL,0).Err() // SET (context, key, val ,experation) so 0 no experation
+	err = s.Client.Set(ctx,shorturl , originalURL,0).Err() // SET (context, key, val ,experation) so 0 no experation
 	if err != nil {
 		return "" , err
 	}
 	
-	return hashURLtoString, nil
+	return shorturl, nil
 }
 
 func (s *RedisStore) GetOriginURL(shortURL string) (string, error){
