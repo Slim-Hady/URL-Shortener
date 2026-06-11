3ced234f9991625ee61a7d43da857bafea7afa71
Slim-Hady <mohamed.hady.dev@gmail.com>
2026-06-11 16:43:49 +0300

    feat: Add Redis Store and hashing function to save orginal URL

--- go.mod
+++ go.mod
@@ -1,10 +1,12 @@
 module URL-SHORTENER
 
-go 1.22.5
+go 1.24
 
 require (
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/redis/go-redis/v9 v9.20.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
 )
--- storage/redis-store.go
+++ storage/redis-store.go
@@ -1,1 +1,49 @@
 package storage
+
+import (
+	"context"
+	"hash/fnv"
+	// "net/url"
+	// "strings"
+	"fmt"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var ctx = context.Background()
+
+type RedisStore struct{
+	Client *redis.Client
+}
+
+func NewRedisStore() *RedisStore {
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+		Protocol: 2,
+	})
+
+	return &RedisStore{Client: rdb};
+}
+
+func (s *RedisStore) SaveURL(originalURL string) (string, error){
+	h := fnv.New64a()
+	h.Write([]byte(originalURL))
+	//fmt.Print("The hashing using sum64 ",h.Sum64(), "\n")
+	hashURLtoString := fmt.Sprintf("%x" , h.Sum64()); // this is using convert to Hex string, Conver to decimal using '%d' 
+
+	err := s.Client.Set(ctx, originalURL,hashURLtoString,0).Err()
+	if err != nil {
+		return "" , err
+	}
+
+	err = s.Client.Set(ctx,hashURLtoString , originalURL,0).Err() // SET (context, key, val ,experation) so 0 no experation
+	if err != nil {
+		return "" , err
+	}
+	
+	return hashURLtoString, nil
+}
+
